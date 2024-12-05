57f4a2e04e946e6fe3d4716c18c3a6cb5ea56b3e
Raz <retraz123@gmail.com>
2024-12-05 09:22:06 +0200

    add views to menu & add option to show all categories by restaurant

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from menu.models import Category
+from rest_framework.response import Response
+from menu.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=False, methods=["get"], url_path="by-restaurant/(?P<restaurant_id>[^/.]+)")
+    def by_restaurant(self, request, restaurant_id=None):
+        categories = self.queryset.filter(restaurant_id=restaurant_id)
+        serializer = self.get_serializer(categories, many=True)
+        return Response(serializer.data)
