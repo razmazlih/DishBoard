315efe84891e328dfbe685927ec0f1c7fb8629e3
Raz <retraz123@gmail.com>
2024-12-05 11:27:12 +0200

    update views, urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import RestaurantViewSet
+from .views import OpeningHouersViewSet, RestaurantViewSet
 
 router = DefaultRouter()
-router.register(r'', RestaurantViewSet, basename='restaurant')
+router.register(r'info', RestaurantViewSet, basename='restaurant')
+router.register(r'opening-houers', OpeningHouersViewSet, basename='opening_houers')
 
 urlpatterns = [
     path('', include(router.urls)),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,43 @@
+from django.forms import ValidationError
 from rest_framework.viewsets import ModelViewSet
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from rest_framework.response import Response
+from .models import OpeningHours, Restaurant
+from .serializers import OpeningHoursSerializer, RestaurantSerializer
+
 
 class RestaurantViewSet(ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+    def list(self, request, *args, **kwargs):
+        fields = request.query_params.get("fields", None)
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        if fields:
+            if "id" not in fields:
+                fields = "id," + fields
+
+            fields = fields.split(",")
+            data = [
+                {field: item[field] for field in fields if field in item}
+                for item in serializer.data
+            ]
+            return Response(data)
+
+        return Response(serializer.data)
+
+
+class OpeningHouersViewSet(ModelViewSet):
+    queryset = OpeningHours.objects.all()
+    serializer_class = OpeningHoursSerializer
+
+    def get_queryset(self):
+        restaurant_id = self.request.query_params.get("restaurant_id")
+        if not restaurant_id:
+            raise ValidationError({"detail": "The 'restaurant_id' parameter is required."})
+
+        queryset = super().get_queryset()
+
+        queryset = queryset.filter(restaurant__id=restaurant_id)
+        return queryset
