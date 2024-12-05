eb82f23a196bceeb9cbabca0e66d57d49ec3227d
Raz <retraz123@gmail.com>
2024-12-05 09:23:11 +0200

    add admin.py code

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Item
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "restaurant")
+    search_fields = ("name", "restaurant__name")
+    list_filter = ("restaurant",)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "is_available", "description")
+    search_fields = ("name", "category__name")
+    list_filter = ("category", "is_available")
+    ordering = ("category", "name")
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Restaurant, OpeningHours
 
-# Register your models here.
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "address")
+    search_fields = ("name", "city", "address")
+    list_filter = ("city",)
+    ordering = ("name",)
+
+
+@admin.register(OpeningHours)
+class OpeningHoursAdmin(admin.ModelAdmin):
+    list_display = ("restaurant", "day_of_week", "is_open", "opening_time", "closing_time")
+    search_fields = ("restaurant__name", "day_of_week")
+    list_filter = ("restaurant", "day_of_week", "is_open")
+    ordering = ("restaurant", "day_of_week")
