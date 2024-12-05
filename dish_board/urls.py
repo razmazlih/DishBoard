dcec1ecfa92a540d3c506a86f185c825dfe4e1ed
Raz <retraz123@gmail.com>
2024-12-05 09:17:19 +0200

    update main urls

--- dish_board/urls.py
+++ dish_board/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/restaurants/", include("restaurant.urls")),
-    # path("api/menu/", include("menu.urls")),
+    path("api/menu/", include("menu.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
